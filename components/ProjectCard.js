741140238f0268080502c097acdf1526ea956d79
Sankalpa Neupane <sankalpa115@gmail.com>
2022-07-17 17:28:28 +0545

    changed toaster position

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -17,7 +17,7 @@ function ProjectCard({title,img,desc,tools,livelink}) {
 
   return (
     <main className=' px-6 md:px-20  py-4 flex flex-col md:flex-row items-center md:space-y-0  space-y-14'>
-      <Toaster  position="bottom-right"/>
+      <Toaster  position="bottom-left"/>
         <div className='relative h-72 md:h-[500px] w-3/4 md:w-[50%] mx-auto'>
             <Image alt='' src={img} className='object-cover rounded-md' layout='fill'/>
         </div>
