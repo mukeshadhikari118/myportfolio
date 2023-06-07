e0b5fa0c8f307a4b2097eff3bbd527f6d78a05f3
sankalpaneupane <sneupane@hamropatro.com>
2023-06-07 16:21:19 +0545

    fix: fix experience and resume

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -4,8 +4,10 @@ import 'react-vertical-timeline-component/style.min.css';
 import {StarIcon} from "@heroicons/react/solid";
 import {MenuIcon} from "@heroicons/react/outline";
 import PageIntro from "./PageIntro";
+import {useSelector} from "react-redux";
 
 function Experience(props) {
+    const mode = useSelector((state) => state.theme?.theme);
     return (
         <div>
             <PageIntro
@@ -22,30 +24,34 @@ function Experience(props) {
                     </svg>
                     }
                 >
-                    <h3 className="vertical-timeline-element-title font-bold">Internship</h3>
-                    <p className='font-light'>Frontend Developer</p>
-                    <h4 className="vertical-timeline-element-subtitle text-red-500 font-bold">Hamro Patro Inc.</h4>
-                    <p className='text-gray-700 text-sm'>
-                        <span className='font-bold uppercase text-gray-900 text-base'>Tech stack: </span> Svelte, Tailwind CSS, TypeScript, gRPC
-                    </p>
-                    <p className='text-gray-500 text-sm'>During this 3 months I was involved with the frontend team to make smooth user interface in Hamro Pay, a new payment wallet system in Nepal.</p>
+                   <div className={`${mode ? "bg-gray-800":"bg-white"} text-white p-4`}>
+                       <h3 className="vertical-timeline-element-title font-bold">Internship</h3>
+                       <p className='font-light'>Frontend Developer</p>
+                       <h4 className="vertical-timeline-element-subtitle text-red-500 font-bold">Hamro Patro Inc.</h4>
+                       <p className={` ${mode ? "text-white":"text-gray-900"} text-sm`}>
+                           <span className='font-bold uppercase text-base underline mr-3'> Tech stack : </span>  Svelte, Tailwind CSS, TypeScript, gRPC
+                       </p>
+                       <p className={`${mode? "text-white":"text-gray-500"} text-sm`}>During this 3 months I was involved with the frontend team to make smooth user interface in Hamro Pay, a new payment wallet system in Nepal.</p>
+                   </div>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="Feb 2023 - current"
+                    date="Feb 2023 - Current"
                     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
                     icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                     </svg>
                     }
                 >
-                    <h3 className="vertical-timeline-element-title font-bold">Associate Software Engineer</h3>
-                    <p className='font-light'>Frontend Developer</p>
-                    <h4 className="vertical-timeline-element-subtitle text-red-500 font-bold">Hamro Patro Inc.</h4>
-                    <p className='text-gray-700 text-sm'>
-                        <span className='font-bold uppercase text-gray-900 text-base'>Tech stack: </span> Svelte, Tailwind CSS, TypeScript, gRPC, React.js, Ant Design, Apex charts
-                    </p>
-                    <p className='text-gray-500 text-sm'>Currently I am continuing in Hamro Patro fintech team where I am responsible in the frontend works in Hamro Pay.</p>
+                    <div className={`${mode ? "bg-gray-800":"bg-white"} text-white p-4`}>
+                        <h3 className="vertical-timeline-element-title font-bold">Associate Software Engineer</h3>
+                        <p className='font-light'>Frontend Developer</p>
+                        <h4 className="vertical-timeline-element-subtitle text-red-500 font-bold">Hamro Patro Inc.</h4>
+                        <p className={` ${mode ? "text-white":"text-gray-900"} text-sm`}>
+                            <span className='font-bold uppercase text-base underline mr-3'>Tech stack : </span> Svelte, Tailwind CSS, TypeScript, gRPC, React.js, Ant Design, Apex charts
+                        </p>
+                        <p  className= {`${mode? "text-white":"text-gray-500"} text-sm`}>Currently I am continuing in Hamro Patro fintech team where I am responsible in the frontend works in Hamro Pay.</p>
+                    </div>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
