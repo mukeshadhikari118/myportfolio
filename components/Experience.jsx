e026e19546364f12ad0be9df3b837028acd5387f
sankalpaneupane <sneupane@hamropatro.com>
2023-06-09 09:18:42 +0545

    fix: env variables

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import {StarIcon} from "@heroicons/react/solid";
-import {MenuIcon} from "@heroicons/react/outline";
 import PageIntro from "./PageIntro";
 import {useSelector} from "react-redux";
 
