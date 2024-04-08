c77f512e5de512cc6882b2484af317494d232531
sankalpaneupane <sneupane@hamropatro.com>
2024-04-08 17:22:37 +0545

    feat: seo

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,15 @@ export default function Home() {
     <main className={`${mode ? "bg-gray-900 text-white" : "bg-white"} snap-y`}>
       <Head>
         <title>Sankalpa Neupane</title>
-          <meta name='Sankalpa Neupane'
+          <meta name='description'
                 content='Discover the creative journey of Sankalpa Neupane through a captivating portfolio. From imaginative web designs to intricate software projects, delve into the fusion of artistry and technology. With a commitment to excellence and a passion for innovation, explore how Sankalpa Neupane brings ideas to life, leaving a lasting impact in the world of design and technology.'>
           </meta>
+          <meta property="og:type" content='website' />
+          <meta property="og:description" content='Discover the creative journey of Sankalpa Neupane through a captivating portfolio. From imaginative web designs to intricate software projects, delve into the fusion of artistry and technology. With a commitment to excellence and a passion for innovation, explore how Sankalpa Neupane brings ideas to life, leaving a lasting impact in the world of design and technology.' />
+          <meta property="og:url" content='https://www.sankalpaneupane.com'/>
+          <meta property="og:image" content='/pp2.webp' />
+          <link rel="canonical" href='https://www.sankalpaneupane.com' />
+          <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <Introduction />
