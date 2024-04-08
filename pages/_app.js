da77292a0e37d33b4bc93f3cb96ee6fd1cbed3b1
sankalpaneupane <sneupane@hamropatro.com>
2024-04-08 20:30:56 +0545

    feat: seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
-
+import { Analytics } from "@vercel/analytics/react"
 function MyApp({ Component, pageProps }) {
   return(
    <Provider store={store}>
      <Component {...pageProps} />
+       <Analytics />
    </Provider>
   )
 }
