8a52e3225e3c5d1f9987f9d8e92a46a7f24c75a7
sankalpaneupane <sneupane@hamropatro.com>
2024-04-11 12:04:16 +0545

    feat: optimizations

--- components/PageIntro.js
+++ components/PageIntro.js
@@ -9,7 +9,7 @@ function PageIntro({ title, description }) {
         mode
           ? "text-white"
           : "  const mode = useSelector(state=>state.theme?.theme)"
-      } items-center flex flex-col space-y-4  my-24`}
+      } items-center flex flex-col space-y-4  md:my-24 my-14`}
     >
       <h2 className="uppercase text-4xl selection:bg-yellow-400 tracking-widest font-extrabold">
         {title}
